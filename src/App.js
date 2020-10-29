004f4fea20e402aff48fc13867321a6a4c1b9cc2
Ahmed Ismail <41244244+AHMYOUSS@users.noreply.github.com>
2020-10-29 05:05:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Task from './Component/Task';
+import { Button } from '@material-ui/core';
+
+
 
 function App() {
+const [taskName, setTaskName ] = useState("");
+const [time, setTime ] = useState("");
+const [taskList, setTaskList ] = useState([]);
+
+const addTask = () => {
+  setTaskList([...taskList , { task : taskName, time : time } ])
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"  >
+      <h1> Todo List </h1>
+      <label> Task Name: </label>
+      <input
+       type="text"
+       id="task"
+        onChange={ (e) => {
+        setTaskName(e.target.value)
+      }} 
+      />
+
+        <label> Time :  </label>
+      <input type="text"
+       id="time"
+        onChange={ (e) => {
+        setTime(e.target.value) }}
+         /> 
+      <Button color="primary" onClick={addTask} > Add </Button>
+      
+    
+         {taskList.map((task) => {
+          return <Task taskName = {task.task} time = {task.time} />
+        } )}
+
     </div>
   );
 }
